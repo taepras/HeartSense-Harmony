4332ad664fbe1d6cb3b38beb6c9abf4f5192b7d4
Tae Prasongpongchai <taepras@gmail.com>
2019-12-14 17:48:43 -0500

    add documentation

--- heartsense_arduino_client/HeartSenseWifiConfig.h
+++ heartsense_arduino_client/HeartSenseWifiConfig.h
@@ -2,7 +2,7 @@
 #define HEARTSENSE_CONFIG_H
 
 const char* WIFI_SSID = "TaeBook";
-const char* WIFI_PWD = "0867802042";
+const char* WIFI_PWD = "123456";
 const char* SERVER_IP = "192.168.137.1";
 const int SERVER_PORT = 3000;
 
